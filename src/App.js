0482ba63543ac20edfe5eab5ad65cf1c8eb717cc
yoon <parkyoon97@gmail.com>
2023-02-22 08:01:03 +0000

    Update 7 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import "./styles.css";
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
+import "./App.css";
 
-export default function App() {
+function App() {
+  console.log("APP RUNNING");
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
+
+export default App;
